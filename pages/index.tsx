77c4ec5504b8e24609f4610f69fa7a049b9ae9a0
kirtarget <sheldongriffiths1@gmail.com>
2022-08-12 03:09:33 +0300

    Add basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div className="text-2xl text-red-200">Hello World!</div>
+  return (
+    <div className=" h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center ">Какой покемон более круглый?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  )
 }
 
 export default Home
